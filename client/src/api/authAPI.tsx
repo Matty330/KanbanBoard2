c5bf98aa620afdbae096ce74d46acd6876cec1ae
Matt <mwelc64@yahoo.com>
2025-02-03 19:43:28 -0500

    Complete login function for authentication

--- client/src/api/authAPI.tsx
+++ client/src/api/authAPI.tsx
@@ -1,9 +1,18 @@
-import { UserLogin } from "../interfaces/UserLogin";
+export const login = async (username: string, password: string) => {
+  try {
+      const response = await fetch("/api/auth/login", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ username, password }),
+      });
 
-const login = async (userInfo: UserLogin) => {
-  // TODO: make a POST request to the login route
-}
+      if (!response.ok) {
+          throw new Error("Invalid username or password");
+      }
 
-
-
-export { login };
+      return response.json();
+  } catch (error) {
+      console.error(error);
+      throw error;
+  }
+};
