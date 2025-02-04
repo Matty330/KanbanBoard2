ba79aceb2a54a981b125ddc93bf14c924b3fb74a
Matt <mwelc64@yahoo.com>
2025-02-03 19:49:57 -0500

    Fix import paths for Board and LoginPage components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,18 @@
-import { Outlet } from 'react-router-dom';
+import { AuthService } from "./utils/auth";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+import Board from "./pages/Board";  // <-- Fix this import
+import LoginPage from "./pages/Login";  // <-- Fix this import
 
-import Navbar from './components/Navbar';
+const App = () => {
+    return (
+        <Router>
+            <Route path="/login" component={LoginPage} />
+            <Route
+                path="/"
+                render={() => (AuthService.isAuthenticated() ? <Board /> : <Redirect to="/login" />)}
+            />
+        </Router>
+    );
+};
 
-function App() {
-
-  return (
-    <div className='container'>
-      <Navbar />
-      <main>
-        <Outlet />
-      </main>
-    </div>
-  )
-}
-
-export default App
+export default App;
