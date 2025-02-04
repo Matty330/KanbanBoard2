71ccd5dfb5078408f53d7f0fcaeea66d3d8fe7e6
Matt <mwelc64@yahoo.com>
2025-02-03 19:58:04 -0500

    Fix unused req parameter in routes

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -7,8 +7,9 @@ const router = express.Router();
 router.use("/auth", authRoutes);
 
 // Protect API routes
-router.use("/tasks", authenticateToken, (req, res) => {
+router.use("/tasks", authenticateToken, (_, res) => {
     res.json({ message: "Authenticated route" });
 });
+
 
 export default router;
